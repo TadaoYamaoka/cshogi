368be250375903f2f1a30df36a532190c51db2dd
TadaoYamaoka <tadaoyamaoka@gmail.com>
2019-02-18 20:31:15 +0900

    名前変更

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@
 from Cython.Build import cythonize
 import numpy
 
-setup(name='cython-shogi',
+setup(name='cshogi',
       ext_modules=cythonize("cshogi.pyx"),
       include_dirs = [numpy.get_include()])
