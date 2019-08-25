9a9fad72b73f07c5d924555752e48b7be206d599
TadaoYamaoka <tadaoyamaoka@gmail.com>
2019-08-25 17:33:52 +0900

    パッケージ指定

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import numpy
 
 setup(name='cshogi',
       version='0.0.1',
-      packages=[''],
+      packages=['cshogi', 'cshogi.usi'],
       ext_modules=cythonize("cshogi/_cshogi.pyx"),
       include_dirs = ["src", numpy.get_include()])
