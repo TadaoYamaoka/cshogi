cdd93de61757da258b16bdb651c9b9195695e780
TadaoYamaoka <tadaoyamaoka@gmail.com>
2020-11-01 11:33:55 +0900

    install_requiresを追加

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 ﻿from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import numpy
 
 class my_build_ext(build_ext):
     def build_extensions(self):
@@ -10,12 +9,18 @@ class my_build_ext(build_ext):
 
         build_ext.build_extensions(self)
 
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 ext_modules = [
     Extension('cshogi._cshogi',
         ['cshogi/_cshogi.pyx',
          "src/bitboard.cpp", "src/common.cpp", "src/generateMoves.cpp", "src/hand.cpp", "src/init.cpp", "src/move.cpp", "src/mt64bit.cpp", "src/position.cpp", "src/search.cpp", "src/square.cpp", "src/usi.cpp", "src/book.cpp", "src/mate.cpp"],
         language='c++',
-        include_dirs = ["src", numpy.get_include()],
+        include_dirs = ["src"],
         define_macros=[('HAVE_SSE4', None), ('HAVE_SSE42', None), ('HAVE_AVX2', None)]),
     Extension('cshogi.gym_shogi.envs.shogi_env',
         ['cshogi/gym_shogi/envs/shogi_env.pyx'],
@@ -30,5 +35,8 @@ setup(
     version='0.0.6',
     packages=['cshogi', 'cshogi.usi', 'cshogi.gym_shogi', 'cshogi.gym_shogi.envs'],
     ext_modules=ext_modules,
-    cmdclass={'build_ext': my_build_ext}
+    cmdclass={'build_ext': my_build_ext},
+    install_requires=['numpy', 'Cython'],
+    author='Tadao Yamaoka',
+    url='https://github.com/TadaoYamaoka/cshogi',
 )
