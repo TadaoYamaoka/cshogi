cf713e38ce2d37cac36f856b7a86591bdb335bd0
TadaoYamaoka <tadaoyamaoka@gmail.com>
2021-03-21 18:00:29 +0900

    CSA出力機能追加

--- cshogi/cli.py
+++ cshogi/cli.py
@@ -1,10 +1,14 @@
 ﻿import random
 import math
 import time
+import re
+import os
+import datetime
 from collections import defaultdict
 
 from cshogi import *
 from cshogi.usi import Engine
+from cshogi import CSA
 from cshogi import PGN
 from cshogi.elo import Elo
 
@@ -15,9 +19,36 @@ try:
 except NameError:
     is_jupyter = False
 
+re_usi_info = re.compile('^.*score (cp|mate) ([+\-0-9]+).*pv (.*)$')
+def usi_info_to_csa_comment(board, info):
+    m = re_usi_info.match(info)
+
+    # score
+    if m is None:
+        return None
+    if m[1] == 'cp':
+        score = int(m[2])
+    elif m[1] == 'mate':
+        if m[2][0] == '-':
+            score = -100000
+        else:
+            score = 100000
+    score *= 1 - board.turn * 2
+
+    # pv
+    pv = []
+    board2 = board.copy()
+    for usi_move in m[3].split(' '):
+        move = board2.move_from_usi(usi_move)
+        assert board2.is_pseudo_legal(move), move
+        pv.append(CSA.COLOR_SYMBOLS[board2.turn] + move_to_csa(move))
+        board2.push(move)
+
+    return f"** {score} {' '.join(pv)}"
+
 def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign=None,
          byoyomi=None, btime=None, wtime=None, binc=None, winc=None,
-         draw=256, opening=None, opening_moves=24, opening_seed=None, pgn=None, no_pgn_moves=False, is_display=True, debug=True):
+         draw=256, opening=None, opening_moves=24, opening_seed=None, csa=None, multi_csa=False, pgn=None, no_pgn_moves=False, is_display=True, debug=True):
     engine1 = Engine(engine1, connect=False)
     engine2 = Engine(engine2, connect=False)
 
@@ -26,15 +57,28 @@ def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign
         class Listener:
             def __init__(self, id):
                 self.id = id
+                self.info = self.bestmove = ''
 
             def __call__(self, line):
                 print(self.id + ':' + line)
+                self.info = self.bestmove
+                self.bestmove = line
 
         listener1 = Listener('1')
         listener2 = Listener('2')
     else:
-        listener1 = None
-        listener2 = None
+        class Listener:
+            def __init__(self):
+                self.info = self.bestmove = ''
+
+            def __call__(self, line):
+                self.info = self.bestmove
+                self.bestmove = line
+        listener1 = listener2 = Listener()
+
+    # CSA
+    if csa and multi_csa:
+        csa_exporter = CSA.Exporter(csa, append=True)
 
     # PGN
     if pgn:
@@ -82,9 +126,17 @@ def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign
         moves = []
         usi_moves = []
         repetition_hash = defaultdict(int)
+        if csa:
+            engine_names = [engine.name for engine in engines_order]
+            if not multi_csa:
+                csa_exporter = CSA.Exporter(os.path.join(csa, '+'.join(engine_names) + '+' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.csa'), append=False)
+            csa_exporter.info(board, engine_names, version='V2')
         if opening:
             for move_usi in opening_list[n // 2 % len(opening_list)]:
-                moves.append(board.push_usi(move_usi))
+                move = board.push_usi(move_usi)
+                if csa:
+                    csa_exporter.move(move)
+                moves.append(move)
                 usi_moves.append(move_usi)
                 repetition_hash[board.zobrist_hash()] += 1
                 if board.move_number > opening_moves:
@@ -127,10 +179,12 @@ def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign
                 # go
                 bestmove, _ = engine.go(byoyomi=byoyomi, btime=remain_time[BLACK], wtime=remain_time[WHITE], binc=binc, winc=winc, listener=listener)
 
+                elapsed_time = time.perf_counter() - start_time
+
                 if remain_time[board.turn] is not None:
                     if inc_time[board.turn] is not None:
                         remain_time[board.turn] += inc_time[board.turn]
-                    remain_time[board.turn] -= math.ceil((time.perf_counter() - start_time) * 1000)
+                    remain_time[board.turn] -= math.ceil(elapsed_time * 1000)
 
                     if remain_time[board.turn] <= 0:
                         # 時間切れ負け
@@ -150,6 +204,8 @@ def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign
                 else:
                     move = board.move_from_usi(bestmove)
                     if board.is_legal(move):
+                        if csa:
+                            csa_exporter.move(move, time=int(elapsed_time), comment=usi_info_to_csa_comment(board, listener.info))
                         board.push(move)
                         moves.append(move)
                         usi_moves.append(bestmove)
@@ -196,27 +252,35 @@ def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign
         if not board.is_game_over() and board.move_number > draw:
             win = WIN_DRAW
             print('まで{}手で持将棋'.format(board.move_number - 1))
+            csa_endgame = '%JISHOGI'
         elif is_fourfold_repetition:
             win = WIN_DRAW
             print('まで{}手で千日手'.format(board.move_number - 1))
+            csa_endgame = '%SENNICHITE'
         elif is_nyugyoku:
             win = board.turn
             print('まで{}手で入玉宣言'.format(board.move_number - 1))
+            csa_endgame = '%KACHI'
         elif is_illegal:
             win = opponent(board.turn)
             print('まで{}手で{}の反則負け'.format(board.move_number - 1, '先手' if win == WHITE else '後手'))
+            csa_endgame = '%ILLEGAL_MOVE'
         elif is_repetition_win:
             win = board.turn
             print('まで{}手で{}の反則勝ち'.format(board.move_number - 1, '先手' if win == BLACK else '後手'))
+            csa_endgame = '%+ILLEGAL_ACTION' if board.turn == WHITE else '%-ILLEGAL_ACTION'
         elif is_repetition_lose:
             win = opponent(board.turn)
             print('まで{}手で{}の反則負け'.format(board.move_number - 1, '先手' if win == WHITE else '後手'))
+            csa_endgame = 'ILLEGAL_MOVE'
         elif is_timeup:
             win = opponent(board.turn)
             print('まで{}手で{}の勝ち'.format(board.move_number - 1, '先手' if win == BLACK else '後手'))
+            csa_endgame = '%TIME_UP'
         else:
             win = opponent(board.turn)
             print('まで{}手で{}の勝ち'.format(board.move_number - 1, '先手' if win == BLACK else '後手'))
+            csa_endgame = '%TORYO'
 
         # 勝敗カウント
         if win == WIN_DRAW:
@@ -269,6 +333,10 @@ def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign
             print('Elo difference: {:.1f} +/- {:.1f}, LOS: {:.1f} %, DrawRatio: {:.1f} %'.format(
                 elo.diff(), error_margin, elo.los(), elo.draw_ratio()))
 
+        # CSA
+        if csa:
+            csa_exporter.endgame(csa_endgame)
+
         # PGN
         if pgn:
             if win == BLACK:
@@ -280,6 +348,10 @@ def main(engine1, engine2, options1={}, options2={}, names=None, games=1, resign
             pgn_exporter.tag_pair([engine.name for engine in engines_order], result, round=n+1)
             if not no_pgn_moves:
                 pgn_exporter.movetext(moves)
+
+    # CSA
+    if csa:
+        csa_exporter.close()
 
     # PGN
     if pgn:
@@ -305,6 +377,8 @@ if __name__ == '__main__':
     parser.add_argument('--opening', type=str)
     parser.add_argument('--opening-moves', type=int, default=24)
     parser.add_argument('--opening-seed', type=int)
+    parser.add_argument('--csa', type=str)
+    parser.add_argument('--multi_csa', action='store_true')
     parser.add_argument('--pgn', type=str)
     parser.add_argument('--no-pgn-moves', action='store_true')
     parser.add_argument('--display', action='store_true')
@@ -327,6 +401,6 @@ if __name__ == '__main__':
         args.games, args.resign,
         args.byoyomi, args.btime, args.wtime, args.binc, args.winc,
         args.draw, args.opening, args.opening_moves, args.opening_seed,
-        args.pgn,
-        args.no_pgn_moves,
+        args.csa, args.multi_csa,
+        args.pgn, args.no_pgn_moves,
         args.display, args.debug)
