03f318774e702880bb49ebebb33b9e7a876d6561
George Yoshida <quiver@users.noreply.github.com>
2021-11-24 18:20:00 +0100

    pipインストール時に依存ライブラリがインストールされていなかった問題を修正

--- setup.py
+++ setup.py
@@ -45,4 +45,5 @@ setup(
         'License :: OSI Approved :: GNU General Public License (GPL)',
         "Operating System :: OS Independent",
     ],
+    install_requires=['flask', 'numpy', 'portpicker'],
 )
