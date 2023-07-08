602f8889fe53abeb71c5d5fad8c6e25e0916175b
TadaoYamaoka <tadaoyamaoka@gmail.com>
2023-07-08 15:57:59 +0900

    is_pseudo_legalで不成を生成

--- src/cshogi.h
+++ src/cshogi.h
@@ -229,7 +229,7 @@ public:
 		return mateMoveInEvenPly(pos, ply);
 	}
 	unsigned long long getKey() const { return pos.getKey(); }
-	bool moveIsPseudoLegal(const int move) const { return pos.moveIsPseudoLegal(Move(move)); }
+	bool moveIsPseudoLegal(const int move) const { return pos.moveIsPseudoLegal<false>(Move(move)); }
 	bool moveIsLegal(const int move) const { return pos.moveIsLegal(Move(move)); }
 	bool is_nyugyoku() const { return nyugyoku(pos); }
 	bool isOK() const { return pos.isOK(); }
