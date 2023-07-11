8754323897fb3e5123f3c425664ac813faa0ed33
TadaoYamaoka <tadaoyamaoka@gmail.com>
2023-07-11 23:44:54 +0900

    v0.5.9 - [usi] infoコマンドの内容を取得するためのリスナークラス追加

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ ext_modules = [
 
 setup(
     name='cshogi',
-    version='0.5.8',
+    version='0.5.9',
     packages=['cshogi', 'cshogi.usi', 'cshogi.gym_shogi', 'cshogi.gym_shogi.envs', 'cshogi.dlshogi', 'cshogi.web', 'cshogi.web.templates', 'cshogi.web.static'],
     package_data={'cshogi.web.templates': ['*'], 'cshogi.web.static': ['*']},
     ext_modules=ext_modules,
