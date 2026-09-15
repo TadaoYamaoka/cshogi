47564dff4a51bfcaa8222fcb4441b315d61895d4
TadaoYamaoka <tadaoyamaoka@gmail.com>
2026-09-15 23:37:25 +0900

    v1.0.5 - mateMoveIn1PlyのAdditional部分に複数の境界条件バグがある #62 - mateMoveを最適化 #65

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ extras_require = {
 
 setup_kwargs = {
     "name": "cshogi",
-    "version": "1.0.4",
+    "version": "1.0.5",
     "description": "A fast Python shogi library",
     "long_description": None,
     "author": "Tadao Yamaoka",
